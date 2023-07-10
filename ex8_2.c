feb302ed011826ba260f0bd7a160ce1d9b6f1bc6
Kazuma Nishimura <106137507+w4mally@users.noreply.github.com>
2023-07-11 04:26:09 +0900

    Add files via upload

---
+++ ex8_2.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define BUF_SIZE	255
+#define ZIPCHAR_SIZE	8
+#define PREF_SIZE	20
+#define CITY_SIZE	50
+#define ADDR_SIZE	100
+#define FULL_ADDR_SIZE	170
+#define SIZE 1024
+
+typedef struct _zipnode{
+    int zip;
+    char fulladdr[FULL_ADDR_SIZE];
+    struct _zipnode *next;
+} zipnode_t;
+
+int skip_delim(char instr[], char delim, int choffset);
+int csv_dq_read(char instr[], char outstr[], int quote_ch, int choffset);
+int read_from_csv(FILE *infp, zipnode_t **table);
+int add_node(zipnode_t **head, int zip, char *fulladdr);
+int print_n_node(zipnode_t *head, int n);
+zipnode_t *search_node(zipnode_t *head, char *fulladdr);
+unsigned int hash(char *str);
+
+zipnode_t *table[SIZE];
+
+int main(void){
+    for(int i=0;i<SIZE;i++){
+        table[i] = NULL;
+    }
+    
+    zipnode_t *result;
+    zipnode_t *node;
+    FILE *infp;
+    int n = 0;
+    char input_addr[FULL_ADDR_SIZE];
+
+	if ( ( infp = fopen("tokyo_all_dat.txt", "r")) == NULL) {
+		fprintf(stderr, "File not found\n");
+		exit(EXIT_FAILURE);
+	}
+
+    n = read_from_csv(infp, table);
+
+    fclose(infp);
+
+    printf("Read %d data\n", n);
+
+    for(int i=0;i<3;i++){
+        printf("data with hash(fulladdr) = %d\n", i);
+        print_n_node(table[i], 5);
+    }
+
+    while(1){
+        printf("full address ? >");
+        fgets(input_addr, sizeof(input_addr), stdin);
+        input_addr[strlen(input_addr)-1] = '\0';
+
+        if(strcmp(input_addr, "exit") == 0){
+            printf("Bye!");
+            break;
+        }
+
+        result = search_node(table[hash(input_addr)], input_addr);
+        if(result == NULL) printf("no data\n");
+        else printf("zipcode = %d\n", (*result).zip);
+    }
+
+    for(int i=0;i<SIZE;i++){
+        while(table[i] != NULL){
+            node = (*table[i]).next;
+            free(table[i]);
+            table[i] = node;
+        }
+    }
+
+    return 0;
+}
+
+int skip_delim(char instr[], char delim, int choffset) {
+	// printf("skip_delim : offset %d, str %s\n", choffset, &instr[choffset]);
+	if (instr[choffset]==delim) {
+		choffset++;
+	} else {
+		fprintf(stderr, "Error at csv_read. Delim Char not found.\n");
+	   	exit(-1);
+	}
+	return (choffset);
+}
+
+int csv_dq_read(char instr[], char outstr[], int quote_ch, int choffset) {
+	int instr_pos, outstr_pos;
+
+	instr_pos = choffset;
+	outstr_pos = 0;
+	// printf("csv_dq_read : offset %d, str %s\n", choffset, &instr[choffset]);
+    if (instr[instr_pos] == quote_ch) {
+    	instr_pos++;
+    } else {
+    	fprintf(stderr, "Error at csv_read. Quote Char not found.\n");
+	   	exit(-1);
+    }
+	while (instr[instr_pos]!=quote_ch) {
+ 		if (!isprint(instr[instr_pos])) {
+			fprintf(stderr, "Error at csv_read. Wrong char.\n");
+		   	exit(-1);
+		} else {
+			outstr[outstr_pos]=instr[instr_pos];
+			outstr_pos++;
+		}
+		instr_pos++;
+	}
+	outstr[outstr_pos]=0; // outstr_pos++;
+	if (instr[instr_pos]==quote_ch)	/* skip quote char */
+		instr_pos++;
+	return (instr_pos);
+}
+
+int read_from_csv(FILE *infp, zipnode_t **table){
+	int chofs, zip,hashed=0;
+    int n = 0;
+	char instr[BUF_SIZE];
+	char zip_char[ZIPCHAR_SIZE];
+	char pref[PREF_SIZE];
+	char city[CITY_SIZE];
+	char addr[ADDR_SIZE];
+
+    char fulladdr[FULL_ADDR_SIZE];
+
+
+	while (fgets(instr, BUF_SIZE, infp) != NULL) {
+		chofs = 0;
+		chofs = csv_dq_read(instr, zip_char, '"', chofs);
+		zip = atoi(zip_char);
+		chofs = skip_delim(instr, ',', chofs);
+		chofs = csv_dq_read(instr, pref, '"', chofs);
+        strcpy(fulladdr, pref);
+        strcat(fulladdr, " ");
+		chofs = skip_delim(instr, ',', chofs);
+		chofs = csv_dq_read(instr, city, '"', chofs);
+		strcat(fulladdr, city);
+        strcat(fulladdr, " ");
+		chofs = skip_delim(instr, ',', chofs);
+		chofs = csv_dq_read(instr, addr, '"', chofs);
+		strcat(fulladdr, addr);
+
+        hashed = hash(fulladdr);
+
+        add_node(&table[hashed], zip, fulladdr);
+
+		n++;
+	}
+	return n;
+}
+
+int add_node(zipnode_t **head, int zip, char *fulladdr){
+    zipnode_t *newnode = (zipnode_t *)malloc(sizeof(zipnode_t));
+    (*newnode).zip = zip;
+    strcpy((*newnode).fulladdr, fulladdr);
+    (*newnode).next = *head;
+    if(newnode == NULL){
+        return 1;
+    }
+    *head = newnode;
+    return 0;
+}
+
+int print_n_node(zipnode_t *head, int n){
+    zipnode_t *node = head;
+    int print_size = 0;
+    int flag=0;
+    for(int i=0;i<n;i++){
+        if(node == NULL){
+            print_size = i+1;
+            flag = 1;
+            break;
+        }
+        printf("%d : %s\n", (*node).zip, (*node).fulladdr);
+        node = (*node).next;
+    }
+    if(flag == 0){
+        return n;
+    }
+    else if(flag == 1){
+        return print_size;
+    }
+}
+
+zipnode_t *search_node(zipnode_t *head, char *fulladdr){
+    zipnode_t *node = head;
+    while(node != NULL){
+        if(strcmp((*node).fulladdr, fulladdr) == 0){
+            return node;
+        }
+        node = (*node).next;
+    }
+    return NULL;
+}
+
+unsigned int hash(char *str){
+    int hashval = 0;
+    while(*str != '\0'){
+        hashval = hashval + *str;
+        str++;
+    }
+    return hashval%SIZE;
+}
