486e13b3c9bf3f1a64c932ea20e31d860fb30afa
Kazuma Nishimura <106137507+w4mally@users.noreply.github.com>
2023-07-13 14:48:50 +0900

    Update ex9_1_1.c

--- ex9_1_1.c
+++ ex9_1_1.c
@@ -7,15 +7,15 @@
 
 int main(void){
     FILE *fp;
-    int tmp = 0;
+    int x = 0;
 
     fp = fopen("ex9_1.txt", "w");
 
     srand(1); /*乱数初期化*/
 
     for(int i=0;i<NUMBER_OF_TIMES;i++){
-        tmp = (int)(rand()*(MAX - MIN + 1) / (1 + RAND_MAX));
-        fprintf(fp, "%d\n", tmp);
+        x = (int)(rand()*(MAX - MIN + 1) / (1 + RAND_MAX));
+        fprintf(fp, "%d\n", x);
     }
 
     fclose(fp);
