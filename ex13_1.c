f90b732d8a7ee95a8c50b96242b3f2a1f07e1c08
Kazuma Nishimura <106137507+w4mally@users.noreply.github.com>
2023-07-26 04:51:05 +0900

    Add files via upload

---
+++ ex13_1.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "hugeint.h"
+
+int main(int argc, char *argv[]){
+    huge_int *x = from_uchar(0); /*argv[1]の格納先*/
+    huge_int *y = from_uchar(0); /*argv[2]の格納先*/
+    huge_int *ten = from_uchar(10);
+    unsigned long long tmp_ull = 0;
+    char rev_ans[10000];
+    char ans_char[10000];
+
+    /*1つ目の引数のhugeintへの変換*/
+    for(int i=0;i<strlen(argv[1]);i++){
+        huge_int *tmp = from_uchar((unsigned char)(argv[1][i] - '0')); /*今回の桁の一時保存先*/
+        huge_int *mul10 = huge_multiply(x, ten); /*ここまでの結果を10倍*/
+        free(x);
+        x = huge_add(mul10, tmp);
+        free(tmp);
+        free(mul10);
+    }
+
+    /*2つ目の引数のhugeintへの変換*/
+    for(int i=0;i<strlen(argv[2]);i++){
+        huge_int *tmp = from_uchar((unsigned char)(argv[2][i] - '0')); /*今回の桁の一時保存先*/
+        huge_int *mul10 = huge_multiply(y, ten); /*ここまでの結果を10倍*/
+        free(y);
+        y = huge_add(mul10, tmp);
+        free(tmp);
+        free(mul10);
+    }
+    
+    huge_int *ans = huge_add(x, y); /*和の計算*/
+    free(x);
+    free(y);
+
+    /*計算結果をchar配列に書き込んで文字列化していく、ただしこの時点では正しい出力と反転している*/
+    int cnt = 0;
+    while(1){
+        if(is_zero(ans)) break;
+        huge_pair tmp_div = huge_divide(ans, ten);
+        tmp_ull = to_ulonglong(tmp_div.r);
+        rev_ans[cnt] = "0123456789"[tmp_ull%10];
+        free(ans);
+        ans = tmp_div.q;
+        free(tmp_div.r);
+        cnt++;
+    }
+
+    rev_ans[cnt] = '\0';
+
+    /*rev_ansを反転させて正しい出力をans_charに書き込む*/
+    for(int i=0;i<cnt;i++){
+        ans_char[i] = rev_ans[cnt-1-i];
+    }
+
+    ans_char[cnt] = '\0';
+
+    printf("%s\n", ans_char);
+
+    free(ten);
+    free(ans);
+
+    return 0;
+}
